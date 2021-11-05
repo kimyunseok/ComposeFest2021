06a3ed8dc4a1f658e25c6a9e4e2cc2474cf7f187
KHS-PC\pskna <psknal27@gmail.com>
2021-11-05 16:23:02 +0900

    feat. Week 1 - 7 : 컴포즈에서의 상태(state)
    - state / mutableStateOf는 특정 값을 가지고 있다가 값이 변하면 UI를 변화시켜준다.
    - remember : state를 기록해둬서, recompose되는 것을 방지한다. 기록은 UI마다 다르게 기록이 된다.

--- week 1-Jetpack Compose basics/Week1/app/src/main/java/com/khs/week1/MainActivity.kt
+++ week 1-Jetpack Compose basics/Week1/app/src/main/java/com/khs/week1/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.khs.week1.ui.theme.Week1Theme
+import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,22 +50,29 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
  * Week1 - 6 열과 행 만들기 : 열 만들기
  * Week1 - 6 열과 행 만들기 : 만들 뷰의 안쪽과 수평방향에 패딩 주는 법
  * Week1 - 6 열과 행 만들기 : 버튼 생성하기. Row가 Horizontal LinearLayout, Column이 Vertical LinearLayout이다.
+ * Week1 - 7 컴포즈에서의 상태 : state / mutableStateOf - 특정 값을 가지고 있다가 값이 변하면 UI를 변화시켜줌. /
+ * Week1 - 7 컴포즈에서의 상태 : remember - state를 기록해 둠으로써 state를 유지시켜준다. 각기 다른 UI마다 다른 값을 기록해둔다.
  * */
 @Composable
 private fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
+
     Surface(color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1F)) {
+            Column(modifier = Modifier
+                .weight(1F)
+                .padding(bottom = extraPadding)) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
             OutlinedButton(
                 onClick = {
-                    //TODO
+                    expanded.value = expanded.value.not()
                 }
             ) {
-                Text(text = "Show Less")
+                Text( if(expanded.value) "Show less" else "Show More")
             }
             
         }
