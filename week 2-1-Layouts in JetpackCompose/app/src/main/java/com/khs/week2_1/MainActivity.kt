53b5af6507742772acff0966f2b1b81bf9938a54
KHS-PC\pskna <psknal27@gmail.com>
2021-11-09 13:33:03 +0900

    Week 2-1 - 3. 수정자
    - 수정자의 속성을 설정할 때는 순서가 중요하다.

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/khs/week2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/khs/week2_1/MainActivity.kt
@@ -3,11 +3,23 @@ package com.khs.week2_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.khs.week2_1.ui.theme.Week2_1Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +29,7 @@ class MainActivity : ComponentActivity() {
             Week2_1Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PhotographerCard()
                 }
             }
         }
@@ -34,5 +46,51 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     Week2_1Theme {
         Greeting("Android")
+    }
+}
+
+/**
+ * Week 2-1 - 3. 수정자 : 수정자에서는 순서에 따라서 뷰가 달라진다.
+ * */
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+//            .padding(16.dp)
+//            .clickable(onClick = {
+//                //클릭시
+//            })
+            //위와같은 순서로 할 경우 패딩 부분에는 온클릭이 적용되지 않는다 !
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = {
+                //클릭시
+            })
+            .padding(16.dp)
+    ) {
+        Surface(modifier = Modifier.size(50.dp), shape = CircleShape, color = MaterialTheme.colors.onSurface.copy(alpha = 0.2F)) {
+            //Image 들어갈 곳
+        }
+
+        Column (
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+                ){
+            Text("Alfred Sisely", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                //Week 2-1 - 3. 수정자 : CompositionLocalProvider를 통해서 글자의 투명도(Alpha) 설정.
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    Week2_1Theme {
+        PhotographerCard()
     }
 }
