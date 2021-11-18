626775ec38e0f819efffc371ec8a81d4b05558c0
KHS-PC\pskna <psknal27@gmail.com>
2021-11-18 17:46:49 +0900

    week 2 - 2 10. Test State in ViewModel

--- week 2-2-Using state in Jetpack Compose/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
@@ -16,6 +16,24 @@
 
 package com.codelabs.state.todo
 
+import com.codelabs.state.util.generateRandomTodoItem
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
 class TodoViewModelTest {
-    // TODO: Write tests
+    @Test
+    fun whenRemovingItem_updateList() {
+        //before
+        val viewModel = TodoViewModel()
+        val item1 = generateRandomTodoItem()
+        val item2 = generateRandomTodoItem()
+        viewModel.addItem(item1)
+        viewModel.addItem(item2)
+
+        //during
+        viewModel.removeItem(item1)
+
+        //after : item1을 지웠으므로 item2와 같은가?
+        assertThat(viewModel.todoItems).isEqualTo(listOf(item2))
+    }
 }
